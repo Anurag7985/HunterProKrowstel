12dda2be6f2d629087fd5dad6ee3042932b674c4
Anurag7985 <kanurag.ofc@gmail.com>
2024-08-22 20:53:58 +0530

    layout setting

---
+++ src/components/Navbar/Index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Box } from '@chakra-ui/react'
+
+const Navbar: React.FC = () => {
+  return (
+    <Box w='100vw' h='100vh'>
+
+    </Box>
+
+  );
+};
+
+export default Navbar;
