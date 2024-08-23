a97423a31fce3b37109a7c44cdd87bbf493353ed
Anurag7985 <kanurag.ofc@gmail.com>
2024-08-23 14:56:16 +0530

    Create search bar

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Box , IconButton} from '@chakra-ui/react'
 import { CloseIcon, HamburgerIcon } from '@chakra-ui/icons';
 import { ChakraProvider } from '@chakra-ui/react';
 import { useState } from 'react';
-// import Index from '../src/components/Navbar/Index';
+import Index from '../src/components/Navbar/Index';
 
 const App: React.FC = () => {
    const [isSideBarOpen , setSideBarOpen] = useState<Boolean>(true);
@@ -15,10 +15,20 @@ const App: React.FC = () => {
     <Box width='100vw' height='100vh'  display='flex' >
       <Box w={isSideBarOpen ?{lg: "15%"} : "5%"}
          transition="width 0.2s"
-       h='100%' bg='yellow'></Box>
+       h='100%' bg='yellow'>
+       {/* Sidebar */}
+       </Box>
       <Box  w={isSideBarOpen ? { lg: "85%" } : '100%'}
-          transition="margin-left 0.2s" h='100%' bg='purple'></Box>
-      {/* <Index/> */}
+          transition="width 0.2s" h='100%' bg='purple' border='2px' borderColor='black'>
+           <Box bg="aqua"  w='100%' h="15%">
+            <Index/>
+
+           </Box>
+           <Box>
+
+           </Box>
+          </Box>
+      
     </Box>
     <IconButton
             aria-label="Toggle Sidebar"
--- src/components/Navbar/Index.tsx
+++ src/components/Navbar/Index.tsx
@@ -1,9 +1,23 @@
 import React from 'react';
 import { Box } from '@chakra-ui/react'
+import { InputGroup, Input, InputLeftElement } from '@chakra-ui/react';
+import { IoIosSearch } from "react-icons/io";
 
 const Navbar: React.FC = () => {
   return (
-    <Box w='100vw' h='100vh'>
+    <Box w='100%' h='100%' bg='brown' display='flex' justifyContent='center' alignItems='center'>
+      <Box>
+        <InputGroup>
+        <InputLeftElement pointerEvents='none'>
+        <IoIosSearch fontSize='1.5vw'/>
+        </InputLeftElement>
+        <Input placeholder='Search Bar'  bg='#F5F7FA' />
+        </InputGroup>
+       
+      </Box>
+      <Box></Box>
+      
+      
 
     </Box>
 
